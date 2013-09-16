b7523398663df71656d9ea7a7a79aacff5f35cb0
Luke Mallon <luke.mallon@axonista.com>
2013-09-16 17:13:59 +0100

    Updated to use os.walk instead of two different ways to get files and folders. Also using re rather than glob.

--- touchwsgi.py
+++ touchwsgi.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sublime
 import sublime_plugin
 import os
-from glob import glob
+import re
 
 
 class TouchWSGI(sublime_plugin.EventListener):
@@ -11,29 +10,26 @@ class TouchWSGI(sublime_plugin.EventListener):
     Sublime Text Plugin to Touch WSGI Files.
 
     Scans any open folders in the current window when saving a file to see
-    if there is a WSGI file in it. "WSGI file" == *.wsgi or *wsgi.py
+    if there is a WSGI file in it. "WSGI file" == r"^.*[\.]wsgi(\.py)?$"
     """
     def on_post_save(self, view):
-        print "Looking for WSGI Files."
         window = view.window()
         for folder in window.folders():
-            self.check_folder(folder)
-            # self.get_folders(folder)
-        print "Done."
+            self.get_folders(folder)
 
-    def check_folder(self, path):
-        wsgi_files = filter(os.path.isfile, glob(path + "/*.wsgi"))
-        if len(wsgi_files) == 0:
-            wsgi_files = filter(os.path.isfile, glob(path + "/*wsgi.py"))
-        if len(wsgi_files) > 0:
-            print "Found WSGI File(s) in " + path
-            for wsgi_file in wsgi_files:
-                os.utime(wsgi_file, None)
+    def check_files(self, path, files):
+        regex = re.compile(r"^.*[\.]wsgi(\.py)?$")
+        files = [filename for filename in files if regex.match(filename) is not None]
+
+        if len(files) > 0:
+            for filename in files:
+                os.utime(path + '/' + filename, None)
 
     def get_folders(self, path):
         for root, dirnames, filenames in os.walk(path):
             if len(dirnames) > 0:
                 for dirname in dirnames:
+                    self.check_files(root, filenames)
+                    filenames = []
                     new_path = os.path.join(root, dirname)
-                    self.check_folder(new_path)
                     self.get_folders(new_path)
