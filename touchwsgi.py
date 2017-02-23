5a2a1d13ae36999991094ed8a51286922bd6b77c
Luke Mallon <luke.mallon@axonista.com>
2017-02-23 12:28:52 +0000

    Add check for None before checking string

--- touchwsgi.py
+++ touchwsgi.py
@@ -13,16 +13,15 @@ class TouchWSGI(sublime_plugin.EventListener):
     if there is a WSGI file in it. "WSGI file" == r"^.*[\.]wsgi(\.py)?$"
     """
     def on_post_save_async(self, view):
-        #start_time = time.time()
         for folder in view.window().folders():
-            # only check when saved file in project
-            if view.file_name().startswith(folder):
+            filename = view.file_name()
+
+            if filename is not None and filename.startswith(folder):
                 self.get_folders(folder)
-        #print("* All checked")
-        #print("--- %s seconds ---" % (time.time() - start_time))
 
     def get_folders(self, path):
         regex = re.compile(r"^.*\.wsgi(\.py)?$")
+
         for root, dirnames, filenames in os.walk(path):
             for file in filenames:
                 if regex.match(file):
